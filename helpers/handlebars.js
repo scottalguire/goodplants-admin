0c522847efc26fff0ddd04cb7bb9712b55078b4e
Scott Alguire <scott.alguire@gmail.com>
2021-03-22 14:27:37 -0400

    Updating api and views to handle array properties. Adding 'genus' to plant model

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,5 +1,8 @@
 module.exports = {
   has_length: function (el) {
-    return el.length > 1
+    return el.length > 1;
   },
-}
+  array_to_comma_string: function (arr) {
+    return Array.isArray(arr) ? arr.join(", ") : [];
+  },
+};
--- routes/api.js
+++ routes/api.js
@@ -1,57 +1,94 @@
-var express = require("express")
-var router = express.Router()
-const { getPlantsJSON, updatePlantsJSON } = require("../utils")
+var express = require("express");
+var router = express.Router();
+const { getPlantsJSON, updatePlantsJSON } = require("../utils");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "goodplants Admin" })
-})
+  res.render("index", { title: "goodplants Admin" });
+});
 
 router.delete("/delete/:id", function (req, res, next) {
-  if (!req.params.id) res.status(403).send({ message: "An id must be provided to delete a plant." })
+  if (!req.params.id) res.status(403).send({ message: "An id must be provided to delete a plant." });
 
   getPlantsJSON().then((json) => {
-    const matchIndex = json.findIndex((plant) => plant.id === parseInt(req.params.id))
+    const matchIndex = json.findIndex((plant) => plant.id === parseInt(req.params.id));
 
     if (matchIndex === -1) {
-      res.status(404).send({ message: "A plant with that id was not found." })
-      return
+      res.status(404).send({ message: "A plant with that id was not found." });
+      return;
     }
 
-    const newJson = [...json]
-    newJson.splice(matchIndex, 1)
+    const newJson = [...json];
+    newJson.splice(matchIndex, 1);
 
     updatePlantsJSON(newJson).then(() => {
-      res.status(201).send({ message: "Plant successfully deleted" })
-    })
-  })
-})
+      res.status(201).send({ message: "Plant successfully deleted" });
+    });
+  });
+});
 
 router.patch("/update/:id", function (req, res, next) {
-  if (!req.params.id) res.status(403).send({ message: "An id must be provided to delete a plant." })
+  if (!req.params.id) res.status(403).send({ message: "An id must be provided to delete a plant." });
 
   getPlantsJSON().then((json) => {
-    const matchIndex = json.findIndex((plant) => plant.id === parseInt(req.params.id))
+    const matchIndex = json.findIndex((plant) => plant.id === parseInt(req.params.id));
 
     if (matchIndex === -1) {
-      res.status(404).send({ message: "A plant with that id was not found." })
-      return
+      res.status(404).send({ message: "A plant with that id was not found." });
+      return;
     }
 
-    const currentPlant = json[matchIndex]
-    const newPlant = { ...currentPlant, ...req.body }
-    const newJson = [...json]
-    newJson[matchIndex] = newPlant
+    const {
+      plant_name,
+      description,
+      price,
+      family,
+      genus,
+      categories,
+      care,
+      scientific_name,
+      image_url,
+      image_alt,
+    } = req.body;
+
+    const currentPlant = json[matchIndex];
+
+    const newPlant = {
+      id: currentPlant.id,
+      price: price || currentPlant.price,
+      name: plant_name || currentPlant.name,
+      description: description || currentPlant.description,
+      scientific_name: scientific_name || currentPlant.scientific_name,
+      family: family || currentPlant.family,
+      genus: genus || currentPlant.genus,
+      categories: (categories && categories.split(",").map((c) => c.trim())) || currentPlant.categories,
+      care: (care && care.split(",").map((c) => c.trim())) || currentPlant.care,
+      image: { url: image_url || currentPlant.image.url, alt: image_alt || currentPlant.image.alt },
+    };
+
+    const newJson = [...json];
+    newJson[matchIndex] = newPlant;
 
     updatePlantsJSON(newJson).then(() => {
-      res.status(201).send(newPlant)
-    })
-  })
-})
+      res.status(201).send(newPlant);
+    });
+  });
+});
 
 router.post("/create", function (req, res, next) {
   getPlantsJSON().then((json) => {
-    const { plant_name, description, price, family, scientific_name } = req.body
+    const {
+      plant_name,
+      description,
+      price,
+      family,
+      genus,
+      categories,
+      care,
+      scientific_name,
+      image_url,
+      image_alt,
+    } = req.body;
 
     const plant = {
       id: json.length + 1,
@@ -60,17 +97,18 @@ router.post("/create", function (req, res, next) {
       description: description || "",
       scientific_name: scientific_name || "",
       family: family || "",
-      categories: [],
-      care: [],
-      image: { url: "", alt: "" },
-    }
+      genus: genus || "",
+      categories: (categories && categories.split(",").map((c) => c.trim())) || [],
+      care: (care && care.split(",").map((c) => c.trim())) || [],
+      image: { url: image_url || "", alt: image_alt || "" },
+    };
 
-    const dataModified = [...json, plant]
+    const dataModified = [...json, plant];
 
     updatePlantsJSON(dataModified).then(() => {
-      res.status(201).send(plant)
-    })
-  })
-})
+      res.status(201).send(plant);
+    });
+  });
+});
 
-module.exports = router
+module.exports = router;
